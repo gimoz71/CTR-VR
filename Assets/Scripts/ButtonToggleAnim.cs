c86e3cdd0ec679a2f26a58865ff3e476af2167e6
Gianluca Monti <gm3871@gmail.com>
2019-10-10 19:10:28 +0200

    init

---
+++ Assets/Scripts/ButtonToggleAnim.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonToggleAnim : MonoBehaviour
+{
+    private bool toggleState = true;
+    private bool toggleStart = true;
+
+    public Animator object1;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        object1.speed = 1;
+       
+    }
+
+
+    public void ToggleState()
+    {
+        if (toggleStart == true)
+        {
+            object1.SetTrigger("idle");
+            
+            toggleStart = false;
+        }
+
+
+
+        if (toggleState == true)
+        {
+            object1.ResetTrigger("in");
+            object1.SetTrigger("out");
+            
+
+            toggleState = false;
+        }
+        else
+        {
+            object1.SetTrigger("in");
+            object1.ResetTrigger("out");
+            
+
+            toggleState = true;
+        }
+    }
+}
---
+++ Assets/Scripts/audioControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioControl : MonoBehaviour
+{
+
+    public AudioSource rumore;
+    public Text label;
+
+    private bool on = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        label.text = "Avvia";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AudioSwitch()
+    {
+        if (on == false)
+        {
+            //Debug.Log("AVVIATO");
+            rumore.Play();
+            label.text = "Spegni";
+            on = true;
+        } else
+        {
+            //Debug.Log("SPENTO");
+            rumore.Stop();
+            label.text = "Avvia";
+            on = false;
+        }
+    }
+}
