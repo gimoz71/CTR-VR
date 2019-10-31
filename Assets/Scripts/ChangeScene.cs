115ab85af1b947ca5926913130376cb74ece62fa
gimoz71 <gm3871@gmail.com>
2019-10-31 07:42:49 +0100

    modofiche in sede ctr

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public string scene;
+    public string buttonLabel;
+    public Text button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+}
